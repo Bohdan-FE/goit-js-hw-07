bab3bd0254e9f73d54a9e3cd1db911faba3b3ee0
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-06-25 18:52:11 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(obj) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${obj.original}">
+    <img
+      class="gallery__image"
+      src="${obj.preview}"
+      data-source="${obj.original}"
+      alt="${obj.description}"
+    />
+  </a>
+</li>`;
+}
+
+list.insertAdjacentHTML(
+  "beforeend",
+  galleryItems.map((obj) => createMarkup(obj)).join("")
+);
+
+function createMarkupInstance({ source } = {}) {
+  return `
+    <img src="${source}" width="800" height="600">
+    `;
+}
+
+list.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains("gallery__image")) {
+    // findItem(evt.target);
+    const instance = basicLightbox.create(
+      createMarkupInstance(evt.target.dataset)
+    );
+    instance.show();
+
+    document.addEventListener("keydown", (evt) => {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+function createMarkup(obj) {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${obj.original}">
+      <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+   </a>
+</li>`;
+}
 
-console.log(galleryItems);
+list.insertAdjacentHTML(
+  "beforeend",
+  galleryItems.map((obj) => createMarkup(obj)).join("")
+);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
